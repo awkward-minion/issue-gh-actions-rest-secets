c626c34681655400d9ba5c4c5c8be065e74ad610
Lakshmaji <lakshmajim@users.noreply.github.com>
2022-07-22 19:48:35 +0530

    create role

--- lib/issue-gh-actions-rest-secets-stack.ts
+++ lib/issue-gh-actions-rest-secets-stack.ts
@@ -1,16 +1,51 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as iam from 'aws-cdk-lib/aws-iam'
 
 export class IssueGhActionsRestSecetsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'IssueGhActionsRestSecetsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const provider = new iam.OpenIdConnectProvider(this, 'MyProvider', {
+      url: 'https://token.actions.githubusercontent.com',
+      clientIds: ['sts.amazonaws.com']
+    });
+
+    // Change this when running in your own repo
+    const githubOrganization = "awkward-minion"
+    const githubRepo = "issue-gh-actions-rest-secrets"
+
+    const GitHubPrincipal = new iam.OpenIdConnectPrincipal(provider).withConditions({
+      StringLike: {
+        'token.actions.githubusercontent.com:sub': `repo:${githubOrganization}/${githubRepo}:*`,
+      }
+    })
+
+
+    const githubRole = new iam.Role(this, 'GithubIssueReproRole', {
+      assumedBy: GitHubPrincipal,
+      description: 'Role to reproduce issue with rest api',
+      roleName: 'github-issue-test-role',
+      maxSessionDuration: Duration.minutes(20),
+      inlinePolicies: {
+        CdkDeploymentPolicy: new iam.PolicyDocument({
+          assignSids: true,
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: ['sts:AssumeRole'],
+              resources: [`arn:aws:iam::${this.account}:role/cdk-*`]
+            })
+          ]
+        })
+      }
+    })
+
+    new CfnOutput(this, "assume_role", {
+      value:
+        githubRole.roleArn,
+    });
+
   }
 }
